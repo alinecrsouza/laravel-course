f39a695b02c2ef0fb9604ff160ebe9e5c438d40c
alinecrsouza <linecrsouza@gmail.com.br>
2016-11-18 14:36:53 -0200

    Using Laravel Elixir

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -8,14 +8,8 @@
         <meta name="author" content="">
         <title>Home | E-Shop</title>
 
-
-        <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ url('css/font-awesome.min.css') }}" rel="stylesheet">
-        <link href="{{ url('css/prettyPhoto.css') }}" rel="stylesheet">
-        <link href="{{ url('css/animate.css') }}" rel="stylesheet">
-        <link href="{{ url('css/main.css') }}" rel="stylesheet">
-        <link href="{{ url('css/responsive.css') }}" rel="stylesheet">
-
+        <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
+        
 
     </head><!--/head-->
 
@@ -145,13 +139,7 @@
 
         </footer><!--/Footer-->
 
-
-
-        <script src="{{ url('js/jquery.js') }}"></script>
-        <script src="{{ url('js/bootstrap.min.js') }}"></script>
-        <script src="{{ url('js/jquery.scrollUp.min.js') }}"></script>
-        <script src="{{ url('js/jquery.prettyPhoto.js') }}"></script>
-        <script src="{{ url('js/main.js') }}"></script>
+        <script src="{{ elixir('js/all.js') }}"></script>       
 
     </body>
 </html> 
