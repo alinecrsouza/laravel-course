51c87f590a93292f6d8da1c609d5aa369762fcc9
alinecrsouza <linecrsouza@gmail.com.br>
2016-11-22 11:26:29 -0200

    Refactoring | Creating a partial view of Products

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -1,74 +1,25 @@
 @extends('store.store')
 
 @section('categories')
-    @include('store._categories')
+    @include('store.partial.categories')
 @stop
 
 @section('content')
 <div class="col-sm-9 padding-right">
     <div class="features_items"><!--features_items-->
         <h2 class="title text-center">Featured</h2>
-        @foreach($pFeatured as $product)
-        <div class="col-sm-4">
-            <div class="product-image-wrapper">
-                <div class="single-products">
-                    <div class="productinfo text-center">
-                        @if(count($product->images))
-                        <img src="{{ url('uploads/'.$product->images->first()->id.'.'.$product->images->first()->extension)}}" alt="" width="200"/>
-                        @else
-                        <img src="{{ url('images/no-img.png')}}" alt="" width="200"/>
-                        @endif
-                        <h2>$ {{ $product->price }}</h2>
-                        <p>{{ $product->name }}</p>
-                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-crosshairs"></i>More details</a>
-
-                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                    </div>
-                    <div class="product-overlay">
-                        <div class="overlay-content">
-                            <h2>$ {{ $product->price }}</h2>
-                            <p>{{ $product->name }}</p>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-crosshairs"></i>More details</a>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endforeach
+                
+        @include('store.partial.products', ['products' => $pFeatured])
+        
     </div><!--features_items-->
 
 
 
     <div class="features_items"><!--recommended-->
         <h2 class="title text-center">Recommended</h2>
-        @foreach($pRecommended as $product)
-        <div class="col-sm-4">
-            <div class="product-image-wrapper">
-                <div class="single-products">
-                    <div class="productinfo text-center">
-                        @if(count($product->images))
-                        <img src="{{ url('uploads/'.$product->images->first()->id.'.'.$product->images->first()->extension)}}" alt="" width="200"/>
-                        @else
-                        <img src="{{ url('images/no-img.png')}}" alt="" width="200"/>
-                        @endif
-                        <h2>$ {{ $product->price }}</h2>
-                        <p>{{ $product->name }}</p>
-                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-crosshairs"></i>More details</a>
-                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                    </div>
-                    <div class="product-overlay">
-                        <div class="overlay-content">
-                            <h2>$ {{ $product->price }}</h2>
-                            <p>{{ $product->name }}</p>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-crosshairs"></i>More details</a>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endforeach
+        
+        @include('store.partial.products', ['products' => $pRecommended])
+        
     </div><!--recommended-->
 
 </div>
--- resources/views/store/partial/categories.blade.php
+++ resources/views/store/partial/categories.blade.php
@@ -11,9 +11,6 @@
             </div>
             @endforeach
         </div><!--/category-products-->
-
-
-
     </div>
 </div>
 
